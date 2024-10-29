3a60372ce880f6635b34b8031679e5b9f207ee4e
Bajracharya <bajracharyaa@etsu.edu>
2024-10-29 17:03:10 -0400

    Roll the dice program

---
+++ roll-dice.py
@@ -0,0 +1,21 @@
+# generate a pair of random numbers between 1 and 6 
+# display results
+# ask if they want to roll the dice again
+
+import random
+
+def generateRandom():
+    num1 = random.randint(1, 6)
+    return num1
+
+while True:
+    ans = input("Roll the dice(y/n):")
+    if ans == "y":
+        num1 = generateRandom()
+        num2 = generateRandom()
+        num_tuple = (num1, num2)
+        print(num_tuple)
+    else:
+        print("Thank you for playing.")
+        break
+
