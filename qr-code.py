3280730dee4ca60ae30955917b24eb8d4eaa0ee6
Bajracharya <bajracharyaa@etsu.edu>
2024-11-12 07:42:09 -0500

    generate mulitple qr codes

--- qr-code.py
+++ qr-code.py
@@ -3,11 +3,23 @@
 
 import qrcode
 
-url = input("Enter url:")
-file_name = input("Enter file name:")
+number = input("How many qr codes do you want?")
+files = {}
 
-qr = qrcode.QRCode(box_size=10, border=4)
-qr.add_data(url)
-qr.make(fit=True)
-img = qr.make_image(fill_color="black", back_color="white")
-img.save(f"qr-img/{file_name}.png")
+for i in range(int(number)):
+    print(f"Enter details for Qr code no. {i + 1}")
+    url = input("Enter url:")
+    file_name = input("Enter file name:")
+    color = input("Enter the QR code color:")
+    if file_name in files.keys():
+        print("Filename already exists")
+    else:
+        files[file_name] = url
+
+for file_name, url in files.items():
+    qr = qrcode.QRCode(box_size=10, border=4)
+    qr.add_data(url)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save(f"qr-img/{file_name}.png")
+    print(f"{file_name} image is saved.")
